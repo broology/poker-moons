d5ab476ba5b88c1fe281e6b5948884255156af1b
Jordan Emslie <jordems@gmail.com>
2021-11-03 18:53:41 -0700

    build(type): add table types

--- libs/shared/type/src/lib/table.ts
+++ libs/shared/type/src/lib/table.ts
@@ -1,10 +1,28 @@
+import { Player, PlayerId } from "./player";
+
+export type TableId = `table_${string}`;
+
 export interface Table {
-    id: string;
+    id: TableId;
 
     /**
      * The name of the table
      */
     name: string;
 
-    players: string[];
+    /**
+     * The array of playerIds at the table
+     */
+    playerIds: PlayerId[];
+
+    /**
+     * A dictionary of the players sitting at the table
+     */
+    players: Player[];
+
+    /**
+     * The number of rounds played around a game.
+     * - The round ends when someone takes the pot.
+     */
+    roundCount: number;
 }
