e92c32489c2bf0a5e0852f54e03a6c5bde6f29f7
MattBla <35305209+MattBla@users.noreply.github.com>
2021-11-24 14:17:04 -0800

    feat(backend): Created base player controller

---
+++ libs/backend/feature/table/src/lib/player/player.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { JoinTableResponse } from '@poker-moons/shared/type';
+import { PlayerService } from './player.service';
+import { JoinTableRequestValidator,  } from './player.validator';
+
+@Controller('player')
+export class PlayerController {
+    constructor(private readonly playerService: PlayerService) {}
+
+    @Post()
+    create(@Body() dto: JoinTableRequestValidator): JoinTableResponse {
+        return this.playerService.create(dto);
+    }
+}
