bde7914c67a294b0af59281b6b0ccfd47013c24d
MattBla <35305209+MattBla@users.noreply.github.com>
2021-11-24 15:51:56 -0800

    feat(backend): Added leave and get cards to validator

--- libs/backend/feature/table/src/lib/player/player.validator.ts
+++ libs/backend/feature/table/src/lib/player/player.validator.ts
@@ -1,8 +1,26 @@
-import { JoinTableRequest } from '@poker-moons/shared/type';
+import { JoinTableRequest, PLAYER_PREFIX, PlayerId } from '@poker-moons/shared/type';
+import { TABLE_PREFIX, TableId } from '@poker-moons/shared/type';
+import { IsEntityId } from '../../../../../../shared/type/src/lib/id.validator';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class JoinTableRequestValidator implements JoinTableRequest {
     @IsString()
     @IsNotEmpty()
     username!: string;
+}
+
+export class LeaveTableRequestValidator {
+  @IsEntityId(TABLE_PREFIX)
+  tableId!: TableId;
+
+  @IsEntityId(PLAYER_PREFIX)
+  playerId!: PlayerId
+}
+
+export class GetPlayerCardsRequestValidator {
+  @IsEntityId(TABLE_PREFIX)
+  tableId!: TableId;
+
+  @IsEntityId(PLAYER_PREFIX)
+  playerId!: PlayerId
 }
