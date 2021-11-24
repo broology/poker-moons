ce07e815e282ecc29f8a899c65a9eeacc4fa252f
MattBla <35305209+MattBla@users.noreply.github.com>
2021-11-24 15:26:41 -0800

    feat(backend): Added services for leave table and get cards

--- libs/backend/feature/table/src/lib/player/player.service.ts
+++ libs/backend/feature/table/src/lib/player/player.service.ts
@@ -1,9 +1,17 @@
 import { Injectable, NotImplementedException } from '@nestjs/common';
-import { JoinTableRequest, JoinTableResponse, LeaveTableResponse, GetPlayerCardsResponse, PlayerId } from '@poker-moons/shared/type';
+import { JoinTableRequest, JoinTableResponse, LeaveTableResponse, GetPlayerCardsResponse, PlayerId, TableId } from '@poker-moons/shared/type';
 
 @Injectable()
 export class PlayerService {
     create(dto: JoinTableRequest): JoinTableResponse {
+        throw new NotImplementedException();
+    }
+  
+    delete(player_id: PlayerId, table_id: TableId): LeaveTableResponse {
+        throw new NotImplementedException();
+    }
+  
+    getCards(player_id: PlayerId, table_id: TableId): GetPlayerCardsResponse {
         throw new NotImplementedException();
     }
 }
