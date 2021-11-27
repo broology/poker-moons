bd2dcbe8ccc1ffba80458ec3b67737285dfd03c7
jordems <jordems@gmail.com>
2021-11-26 22:36:06 -0800

    chore(frontend): Setup test cases for TTD

--- apps/frontend-e2e/src/integration/app.spec.ts
+++ apps/frontend-e2e/src/integration/app.spec.ts
@@ -1,7 +1,10 @@
-describe('frontend', () => {
-    beforeEach(() => cy.visit('/'));
+/**
+ * Simple test to make sure app is building and rendering
+ */
 
+describe('frontend', () => {
     it('should render page', () => {
+        cy.visit('/');
         cy.get('html');
     });
 });
---
+++ apps/frontend-e2e/src/integration/table-action.spec.ts
@@ -0,0 +1,23 @@
+describe('table action', () => {
+    beforeEach(() => {
+        // * Load game state for player to be active seat
+    });
+
+    it('should perform call', () => {
+        // * Player clicks `call` button
+        // * Correct number of chips are moved into `pot`
+        // * Active turn is moved to next player
+    });
+
+    it('should perform raise', () => {
+        // * Player clicks `raise` button
+        // * Correct number of chips are moved into `pot`
+        // * Active turn is moved to next player
+    });
+
+    it('should perform fold', () => {
+        // * Player clicks `fold` button
+        // * The ui updates to display player has folded
+        // * Active turn is moved to next player
+    });
+});
