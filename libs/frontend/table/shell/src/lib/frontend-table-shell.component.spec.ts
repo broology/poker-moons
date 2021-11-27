db5149e39ab8562c2346e6d5ffc387ea141dae44
jordems <jordems@gmail.com>
2021-11-26 22:03:04 -0800

    build(frontend): Create table shell

--- libs/frontend/table/shell/src/lib/frontend-table-shell.component.spec.ts
+++ libs/frontend/table/shell/src/lib/frontend-table-shell.component.spec.ts
@@ -3,23 +3,22 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FrontendTableShellComponent } from './frontend-table-shell.component';
 
 describe('FrontendTableShellComponent', () => {
-  let component: FrontendTableShellComponent;
-  let fixture: ComponentFixture<FrontendTableShellComponent>;
+    let component: FrontendTableShellComponent;
+    let fixture: ComponentFixture<FrontendTableShellComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ FrontendTableShellComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [FrontendTableShellComponent],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(FrontendTableShellComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(FrontendTableShellComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
