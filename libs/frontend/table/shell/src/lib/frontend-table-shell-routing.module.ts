76e14b2bfe053b6a5da74f3f43181f22391bf3c6
jordems <jordems@gmail.com>
2021-12-04 09:49:37 -0800

    refactor(table-shell): Remove unused data in router

--- libs/frontend/table/shell/src/lib/frontend-table-shell-routing.module.ts
+++ libs/frontend/table/shell/src/lib/frontend-table-shell-routing.module.ts
@@ -6,20 +6,8 @@ import { FrontendTableShellComponent } from './frontend-table-shell.component';
     imports: [
         RouterModule.forChild([
             {
-                path: '',
-                component: FrontendTableShellComponent,
-                data: {
-                    builder: true,
-                },
-            },
-
-            //TODO add guard that will detect if the table exists
-            {
                 path: ':tableId',
                 component: FrontendTableShellComponent,
-                data: {
-                    join: true,
-                },
             },
 
             //TODO 404 table not found page
