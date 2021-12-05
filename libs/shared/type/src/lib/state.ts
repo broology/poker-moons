55480f80f4fa7caaa127f58a8194ee5b42645738
jordems <jordems@gmail.com>
2021-12-04 16:33:14 -0800

    chore(type): shift immutable player split into client table state

--- libs/shared/type/src/lib/state.ts
+++ libs/shared/type/src/lib/state.ts
@@ -1,19 +1,9 @@
-import { ImmutablePublicPlayer } from '..';
+import { ImmutablePublicPlayer, Player } from '..';
 import type { Card } from './card';
 import type { MutablePublicPlayer, PlayerId } from './player';
 import type { Table, TableId } from './table';
 
-export interface SharedTableState extends Pick<Table, 'seatMap' | 'roundCount' | 'activeRound'> {
-    /**
-     * Map of player data that will be frequently changed.
-     */
-    mutablePlayerMap: Record<PlayerId, MutablePublicPlayer>;
-
-    /**
-     * Map of player data that will (for the most part) stay the same during the game
-     */
-    immutablePlayerMap: Record<PlayerId, ImmutablePublicPlayer>;
-}
+export type SharedTableState = Pick<Table, 'seatMap' | 'roundCount' | 'activeRound'>;
 
 export interface ClientTableState extends SharedTableState {
     cards: [Card, Card] | [];
@@ -27,9 +17,20 @@ export interface ClientTableState extends SharedTableState {
      * The `TableId` that is loaded, if null, then the table isn't loaded yet
      */
     tableId: TableId | null;
+
+    /**
+     * Map of player data that will be frequently changed.
+     */
+    mutablePlayerMap: Record<PlayerId, MutablePublicPlayer>;
+
+    /**
+     * Map of player data that will (for the most part) stay the same during the game
+     */
+    immutablePlayerMap: Record<PlayerId, ImmutablePublicPlayer>;
 }
 
 export interface ServerTableState extends SharedTableState {
     deck: Card[];
-    playerCards: Record<PlayerId, [Card, Card] | []>;
+
+    playerMap: Record<PlayerId, Player>;
 }
