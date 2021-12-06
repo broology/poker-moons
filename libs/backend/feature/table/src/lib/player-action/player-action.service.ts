54a1134b52f35cfd5891c9e4ce727c243e85d95d
kevinfeddema <kfeddema25@gmail.com>
2021-12-05 21:10:34 -0700

    feat(backend): added base actions and perform switch

--- libs/backend/feature/table/src/lib/player-action/player-action.service.ts
+++ libs/backend/feature/table/src/lib/player-action/player-action.service.ts
@@ -1,9 +1,44 @@
 import { Injectable, NotImplementedException } from '@nestjs/common';
-import { PerformPlayerActionRequest, PerformPlayerActionResponse } from '@poker-moons/shared/type';
+import {
+    CallPlayerAction,
+    CheckPlayerAction,
+    FoldPlayerAction,
+    PerformPlayerActionRequest,
+    PerformPlayerActionResponse,
+    RaisePlayerAction,
+} from '@poker-moons/shared/type';
 
 @Injectable()
 export class PlayerActionService {
     perform(dto: PerformPlayerActionRequest): PerformPlayerActionResponse {
+        switch (dto.action.type) {
+            case 'fold':
+                this.fold(dto.action);
+                break;
+            case 'call':
+                this.call(dto.action);
+                break;
+            case 'raise':
+                this.raise(dto.action);
+                break;
+            case 'check':
+                this.check(dto.action);
+                break;
+        }
+
         throw new NotImplementedException();
+    }
+
+    fold(action: FoldPlayerAction): PerformPlayerActionResponse {
+        throw new NotImplementedException(action);
+    }
+    call(action: CallPlayerAction): PerformPlayerActionResponse {
+        throw new NotImplementedException(action);
+    }
+    raise(action: RaisePlayerAction): PerformPlayerActionResponse {
+        throw new NotImplementedException(action);
+    }
+    check(action: CheckPlayerAction): PerformPlayerActionResponse {
+        throw new NotImplementedException(action);
     }
 }
