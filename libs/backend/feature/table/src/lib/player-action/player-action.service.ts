734ced4c3623330e0d41e040a802e07da20708a5
kevinfeddema <kfeddema25@gmail.com>
2021-12-14 22:22:18 -0700

    fix(backend): built validator concept

--- libs/backend/feature/table/src/lib/player-action/player-action.service.ts
+++ libs/backend/feature/table/src/lib/player-action/player-action.service.ts
@@ -2,18 +2,23 @@ import { Injectable, NotImplementedException } from '@nestjs/common';
 import {
     CallPlayerAction,
     CheckPlayerAction,
+    ClientTableState,
     FoldPlayerAction,
     PerformPlayerActionRequest,
     PerformPlayerActionResponse,
+    PokerMoonsError,
     RaisePlayerAction,
+    Table,
 } from '@poker-moons/shared/type';
+import { Either, isRight, left, right } from 'fp-ts/lib/Either';
+import { match } from 'ts-pattern';
 
 @Injectable()
 export class PlayerActionService {
     perform(dto: PerformPlayerActionRequest): PerformPlayerActionResponse {
         switch (dto.action.type) {
             case 'fold':
-                this.fold(dto.action);
+                this.fold(this.canFold());
                 break;
             case 'call':
                 this.call(dto.action);
@@ -29,16 +34,57 @@ export class PlayerActionService {
         throw new NotImplementedException();
     }
 
-    fold(action: FoldPlayerAction): PerformPlayerActionResponse {
-        throw new NotImplementedException(action);
+    fold(action: Either<PokerMoonsError, FoldPlayerAction>): PerformPlayerActionResponse {
+        if (isRight(action)) throw new NotImplementedException(action.right);
+        else throw new Error(action.left);
     }
-    call(action: CallPlayerAction): PerformPlayerActionResponse {
-        throw new NotImplementedException(action);
+    call(action: Either<PokerMoonsError, CallPlayerAction>): PerformPlayerActionResponse {
+        if (isRight(action)) throw new NotImplementedException(action.right);
+        else throw new Error(action.left);
     }
-    raise(action: RaisePlayerAction): PerformPlayerActionResponse {
-        throw new NotImplementedException(action);
+    raise(action: Either<PokerMoonsError, RaisePlayerAction>): PerformPlayerActionResponse {
+        if (isRight(action)) throw new NotImplementedException(action.right);
+        else throw new Error(action.left);
     }
-    check(action: CheckPlayerAction): PerformPlayerActionResponse {
-        throw new NotImplementedException(action);
+    check(action: Either<PokerMoonsError, CheckPlayerAction>): PerformPlayerActionResponse {
+        if (isRight(action)) throw new NotImplementedException(action.right);
+        else throw new Error(action.left);
+    }
+
+    canFold(
+        table: Table,
+        clientState: ClientTableState,
+        action: FoldPlayerAction,
+    ): Either<PokerMoonsError, FoldPlayerAction> {
+        return match([])
+            .with([], () => left('Cannot perform action.'))
+            .otherwise(() => right(action));
+    }
+    canCall(
+        table: Table,
+        clientState: ClientTableState,
+        action: CallPlayerAction,
+    ): Either<PokerMoonsError, CallPlayerAction> {
+        return match([])
+            .with([], () => left('Cannot perform action.'))
+            .otherwise(() => right(action));
+    }
+    canRaise(
+        table: Table,
+        clientState: ClientTableState,
+        action: RaisePlayerAction,
+    ): Either<PokerMoonsError, RaisePlayerAction> {
+        return match([])
+            .with([], () => left('Cannot perform action.'))
+            .otherwise(() => right(action));
+    }
+    canCheck(
+        table: Table,
+        clientState: ClientTableState,
+        action: CheckPlayerAction,
+    ): Either<PokerMoonsError, CheckPlayerAction> {
+        return match([])
+            .with([], () => left('Cannot perform action.'))
+            .otherwise(() => right(action));
     }
 }
