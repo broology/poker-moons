312a5e1cd2835ca81365e5ecd1f7f0f22a0e667a
MattBla <35305209+MattBla@users.noreply.github.com>
2021-12-15 15:56:09 -0800

    feat(): registered player module in  BackendFeatureTableRouter as per PR review

--- libs/backend/feature/table/src/lib/backend-feature-table.module.ts
+++ libs/backend/feature/table/src/lib/backend-feature-table.module.ts
@@ -1,8 +1,14 @@
 import { Module } from "@nestjs/common";
 import { RouterModule } from "@nestjs/core";
 import { TableModule } from "./table/table.module";
+import { PlayerModule } from "./player/player.module";
 
 @Module({
-    imports: [TableModule, RouterModule.register([{ path: "table", module: TableModule }])]
+    imports: [TableModule, RouterModule.register([{ path: "table", module: TableModule, children: [{
+	    path: ':tableId', children: [{
+		    path: 'player',
+		    module: PlayerModule
+	    }] 
+    }])]
 })
 export class BackendFeatureTableModule {}
