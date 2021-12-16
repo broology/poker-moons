39c4d9871c5607fe1fc9dfe70f90cb04d3a80764
MattBla <35305209+MattBla@users.noreply.github.com>
2021-12-15 16:18:12 -0800

    fix(): adding player prefix export to player.ts type

--- libs/shared/type/src/lib/player.ts
+++ libs/shared/type/src/lib/player.ts
@@ -3,6 +3,7 @@ import { Card } from './card';
 
 export type PlayerId = `player_${string}`;
 
+export const PLAYER_PREFIX = 'player' as const;
 export const playerStatus = ['waiting', 'checked', 'called', 'folded', 'raised', 'all-in'] as const;
 
 /**
