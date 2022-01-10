d7a1e9b59519a2349e431835aaef2ad2734f7565
jordems <jordems@gmail.com>
2022-01-09 21:23:56 -0800

    fix: dockerfile wasn't pointing to backend

--- tools/executors/docker/build-and-push/impl.ts
+++ tools/executors/docker/build-and-push/impl.ts
@@ -19,19 +19,17 @@ export default async function (
 
     //  Build docker image with linux/arm64, as that's what we are using on elastic beanstalk
     cli(
-        `sudo docker buildx build . -t "${context.projectName}:${options.tag}" --target="${context.projectName}" --memory=500m -f ./apps/${context.projectName}/Dockerfile --platform linux/arm64`,
+        `docker buildx build . --tag="${context.projectName}:${options.tag}" --target="${context.projectName}" --memory=500m -f ./apps/${context.projectName}/Dockerfile --platform=linux/arm64`,
     );
 
     // Before we can push to ecr, we need to authenticate with it
-    cli(
-        `sudo docker login -u AWS -p $(aws ecr get-login-password --region ${options.region}) ${options.ecrRegistryUrl}`,
-    );
+    cli(`docker login -u AWS -p $(aws ecr get-login-password --region ${options.region}) ${options.ecrRegistryUrl}`);
 
     // Tag Image
-    cli(`sudo docker tag "${context.projectName}:${options.tag}" "${appRegistry}:${options.tag}"`);
+    cli(`docker tag "${context.projectName}:${options.tag}" "${appRegistry}:${options.tag}"`);
 
     // Push docker image
-    cli(`sudo docker push "${appRegistry}:${options.tag}"`);
+    cli(`docker push "${appRegistry}:${options.tag}"`);
 
     logger.log(`Docker Build and Push for ${app} was successful`);
 
