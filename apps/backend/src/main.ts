58d59d7aade8a0e7a2f7c3a8ee892556785ed9c6
jordems <jordems@gmail.com>
2022-01-10 21:03:21 -0800

    chore: affect apps

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
     const port = process.env.PORT || 3000;
     await app.listen(port);
-    Logger.log(`🚀 Application is running on: http://localhost:${port}/`);
+    Logger.log(`🚀 Application is running on http://localhost:${port}/`);
 }
 
 bootstrap();
