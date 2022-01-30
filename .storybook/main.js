935d040a35e275bd32d35f35f9e0a6df5f0c7da2
jordems <jordems@gmail.com>
2022-01-30 13:58:58 -0800

    refactor(storybook): swap from knobs to controls lib

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,5 +3,5 @@ module.exports = {
         builder: 'webpack5',
     },
     stories: [],
-    addons: ['@storybook/addon-essentials', '@storybook/addon-knobs'],
+    addons: ['@storybook/addon-essentials', '@storybook/addon-controls'],
 };
