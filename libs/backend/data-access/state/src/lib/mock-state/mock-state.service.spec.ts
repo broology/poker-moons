e5bd3fadcb684a6e6aaefac48459506189bc7d4a
Dylan Baiko <35238045+dBaiko@users.noreply.github.com>
2022-02-17 19:39:36 -0800

    Table state manager tests and updates (#121)
    * feat(mock-state): add tests
    
    * feat(table-state-manager): add tests

--- libs/backend/data-access/state/src/lib/mock-state/mock-state.service.spec.ts
+++ libs/backend/data-access/state/src/lib/mock-state/mock-state.service.spec.ts
@@ -1,45 +1,155 @@
 import { MockStateService } from './mock-state.service';
 import { Test } from '@nestjs/testing';
-import { Player, Round, ServerTableState, TableId } from '@poker-moons/shared/type';
+import { Player, PlayerId, Round, ServerTableState, TableId } from '@poker-moons/shared/type';
+
+const mockPlayer: Player = {
+    id: `player_${'1'}`,
+    username: 'test',
+    img: 'test',
+    stack: 4,
+    status: `waiting`,
+    called: 0,
+    seatId: null,
+    cards: [],
+};
+const mockRound: Round = {
+    pot: 0,
+    toCall: 0,
+    turnCount: 0,
+    roundStatus: 'deal',
+    activeSeat: null,
+    dealerSeat: 0,
+    smallBlind: 0,
+    cards: [],
+};
+const mockTableState: ServerTableState = {
+    name: 'test',
+    deck: [],
+    playerMap: { player_1: mockPlayer },
+    seatMap: { 0: undefined, 1: undefined, 2: undefined, 3: undefined, 4: undefined, 5: undefined },
+    roundCount: 0,
+    activeRound: mockRound,
+};
 
 describe('MockStateService', () => {
     let service: MockStateService;
-    it('should create a table', async () => {
+
+    beforeEach(async () => {
         const module = await Test.createTestingModule({
             providers: [MockStateService],
         }).compile();
         service = module.get<MockStateService>(MockStateService);
-        const mockPlayer: Player = {
-            id: `player_${'1'}`,
-            username: 'test',
-            img: 'test',
-            stack: 4,
-            status: `waiting`,
-            called: 0,
-            seatId: null,
-            cards: [],
-        };
-        const mockRound: Round = {
-            pot: 0,
-            toCall: 0,
-            turnCount: 0,
-            roundStatus: 'deal',
-            activeSeat: null,
-            dealerSeat: 0,
-            smallBlind: 0,
-            cards: [],
-        };
-        const mockTableState: ServerTableState = {
-            name: 'test',
-            deck: [],
-            playerMap: { player_1: mockPlayer },
-            seatMap: { 0: undefined, 1: undefined, 2: undefined, 3: undefined, 4: undefined, 5: undefined },
-            roundCount: 0,
-            activeRound: mockRound,
-        };
-        const id: TableId = await service.create(mockTableState);
-        const id2: TableId = await service.create(mockTableState);
-        console.log(id);
-        console.log(id2);
+    });
+
+    it('should be created', () => {
+        expect(service).toBeDefined();
+    });
+
+    describe('create', () => {
+        it('should create a basic, default table', async () => {
+            const id: TableId = await service.create(mockTableState);
+            const id1: TableId = await service.create(mockTableState);
+            expect(id).toEqual('table_1');
+            expect(id1).toEqual('table_2');
+        });
+    });
+
+    describe('getState', () => {
+        it('should properly retrieve a table', async () => {
+            await service.create(mockTableState);
+            expect(await service.getState('table_1')).toEqual(mockTableState);
+        });
+
+        it('should get the correct table when multiple are created', async () => {
+            await service.create(mockTableState);
+            const mockState: ServerTableState = {
+                name: 'test2',
+                deck: [],
+                playerMap: { player_1: mockPlayer },
+                seatMap: { 0: undefined, 1: undefined, 2: undefined, 3: undefined, 4: undefined, 5: undefined },
+                roundCount: 0,
+                activeRound: mockRound,
+            };
+            await service.create(mockState);
+            const table1: ServerTableState = await service.getState('table_1');
+            expect(table1.name).toEqual('test');
+            const table2: ServerTableState = await service.getState('table_2');
+            expect(table2.name).toEqual('test2');
+        });
+    });
+
+    describe('delete', () => {
+        it('should delete a table', async () => {
+            await service.create(mockTableState);
+            await service.delete('table_1');
+            expect(await service.getState('table_1')).toBeUndefined();
+        });
+    });
+
+    describe('update', () => {
+        it('should update a table - top level', async () => {
+            await service.create(mockTableState);
+            await service.update('table_1', { name: "Your mom's dinner table" });
+            const table: ServerTableState = await service.getState('table_1');
+            expect(table.name).toEqual("Your mom's dinner table");
+        });
+
+        it('should update a table - player', async () => {
+            await service.create(mockTableState);
+            const table: ServerTableState = await service.getState('table_1');
+
+            const playerMap: Record<PlayerId, Player> = table.playerMap;
+            const updatedPlayerMap: Record<PlayerId, Player> = {
+                ...playerMap,
+                player_1: {
+                    ...playerMap.player_1,
+                    username: 'Bob',
+                },
+            };
+            await service.update('table_1', { playerMap: updatedPlayerMap });
+            const updatedTable: ServerTableState = await service.getState('table_1');
+            expect(updatedTable.playerMap.player_1.username).toEqual('Bob');
+        });
+
+        it('should update a table - round', async () => {
+            await service.create(mockTableState);
+            const table: ServerTableState = await service.getState('table_1');
+            await service.update('table_1', { activeRound: { ...table.activeRound, pot: 500 } });
+            const updatedTable: ServerTableState = await service.getState('table_1');
+            expect(updatedTable.activeRound.pot).toEqual(500);
+        });
+
+        it('should update a table - seatmap', async () => {
+            await service.create(mockTableState);
+            const table: ServerTableState = await service.getState('table_1');
+            await service.update('table_1', { seatMap: { ...table.seatMap, 0: 'player_1' } });
+            const updatedTable: ServerTableState = await service.getState('table_1');
+            expect(updatedTable.seatMap['0']).toEqual('player_1');
+        });
+
+        it('should add a new player to playermap', async () => {
+            await service.create(mockTableState);
+            const newPlayer: Player = {
+                id: `player_${'2'}`,
+                username: 'Bob',
+                img: 'test',
+                stack: 4,
+                status: `waiting`,
+                called: 0,
+                seatId: null,
+                cards: [],
+            };
+            const table: ServerTableState = await service.getState('table_1');
+            const playerMap: Record<PlayerId, Player> = table.playerMap;
+            let updatedPlayerMap: Record<PlayerId, Player> = {
+                ...playerMap,
+                [newPlayer.id]: newPlayer,
+            };
+            await service.update('table_1', { playerMap: updatedPlayerMap });
+            const updatedTable: ServerTableState = await service.getState('table_1');
+            updatedPlayerMap = updatedTable.playerMap;
+            expect(updatedPlayerMap.player_2).toBeDefined();
+            expect(updatedPlayerMap.player_2.username).toEqual('Bob');
+        });
     });
 });
