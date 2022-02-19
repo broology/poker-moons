ebfb8c3bfd848633a5bf22c700ce5f19e0a1d6c7
jordems <jordems@gmail.com>
2022-02-19 10:39:36 -0800

    build(backend): connect table feature to app

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { BackendFeatureTableModule } from '@poker-moons/backend/feature/table';
 import { AppController } from './app.controller';
 
 @Module({
+    imports: [BackendFeatureTableModule],
     controllers: [AppController],
 })
 export class AppModule {}
