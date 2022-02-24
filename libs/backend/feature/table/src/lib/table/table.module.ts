521cb31c764e976c40ea6cbd4cc8e2890d860f28
jordems <jordems@gmail.com>
2022-02-24 00:03:33 -0800

    fix(table): add import of table state module

--- libs/backend/feature/table/src/lib/table/table.module.ts
+++ libs/backend/feature/table/src/lib/table/table.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TableStateManagerModule } from '../table-state-manager/table-state-manager.module';
 import { TableController } from './table.controller';
 import { TableService } from './table.service';
 
 @Module({
+    imports: [TableStateManagerModule],
     controllers: [TableController],
     providers: [TableService],
 })
