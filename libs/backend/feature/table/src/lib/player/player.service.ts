d56de00a5c479026c9cb3e4af5865ba60cb25a48
Matt <blanke.matt@gmail.com>
2022-04-20 16:00:32 -0700

    fix(): implemented suggested changes from PR review

--- libs/backend/feature/table/src/lib/player/player.service.ts
+++ libs/backend/feature/table/src/lib/player/player.service.ts
@@ -39,23 +39,21 @@ export class PlayerService {
     async create(dto: JoinTableRequest, tableId: TableId): Promise<JoinTableResponse> {
         this.logger.log("New player has joined table " + tableId);
         const currentSeatMap: Partial<Record<SeatId, PlayerId>> = (await this.tableStateManagerService.getTableById(tableId)).seatMap;
-        const seatArray: Array<SeatId> = Object.keys(currentSeatMap) as Array<unknown> as Array<SeatId>;
-        let firstAvailableSeat = seatArray.find(key => currentSeatMap[key] === undefined);
-        firstAvailableSeat = firstAvailableSeat as SeatId;
-        const newPlayer:Player = {...initialPlayerState, username: dto.username, seatId: firstAvailableSeat};
+        const seatArray: SeatId[] = Object.keys(currentSeatMap) as Array<unknown> as Array<SeatId>;
+        const firstAvailableSeat = seatArray.find(key => currentSeatMap[key] === undefined);
+        const newPlayer:Player = {...initialPlayerState, username: dto.username, seatId: firstAvailableSeat as SeatId};
         await this.tableStateManagerService.addNewPlayerToTable(tableId, newPlayer);
         return newPlayer;
     }
 
     async delete(tableId: TableId, playerId: PlayerId): Promise<LeaveTableResponse> {
+        //TODO: rename to removeFromTable? leave?
         this.logger.log("Player " + playerId + " has left table " + tableId);
         const playerUpdate: Partial<Player> = {
-            //TODO: OK with adding new status for left/out of game?
             status : 'out'
         };
         await this.tableStateManagerService.updateTablePlayer(tableId, playerId, playerUpdate);
-        //TODO: Do we want to actually return the final state of the Player object here? or just a confirmation?
-        return initialPlayerState;
+        return (await this.tableStateManagerService.getTableById(tableId)).playerMap[playerId];
     }
 
     async getCards(tableId: TableId, playerId: PlayerId): Promise<GetPlayerCardsResponse> {
