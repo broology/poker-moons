61160d7bf9d5e980941c191c6fd2576c2e575aa9
kevinfeddema <kfeddema25@gmail.com>
2022-05-08 14:53:49 -0600

    feat(frontend): added props and style getters

--- libs/frontend/shared/ui/src/lib/buttons/button/button.component.ts
+++ libs/frontend/shared/ui/src/lib/buttons/button/button.component.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable @angular-eslint/no-empty-lifecycle-method */
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ButtonColors, ButtonVariant } from './button.type';
 
 @Component({
     selector: 'poker-moons-button',
@@ -8,7 +9,21 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./button.component.scss'],
 })
 export class ButtonComponent implements OnInit {
-    constructor() {}
+    @Input() variant?: ButtonVariant;
+    @Input() color?: ButtonColors;
+
+    constructor() {
+        this.variant = 'primary';
+        this.color = 'primary';
+    }
 
     ngOnInit(): void {}
+
+    getVariantClass() {
+        return this.variant === 'secondary' ? 'variant-secondary' : 'variant-primary';
+    }
+
+    getColorClass() {
+        return this.color;
+    }
 }
