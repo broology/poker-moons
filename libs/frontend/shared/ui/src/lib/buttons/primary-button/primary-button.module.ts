364596dc03006323834906fbd050c8855fa6175c
kevinfeddema <kfeddema25@gmail.com>
2022-05-10 23:00:11 -0600

    fix(frontend): finish primary button styles

--- libs/frontend/shared/ui/src/lib/buttons/primary-button/primary-button.module.ts
+++ libs/frontend/shared/ui/src/lib/buttons/primary-button/primary-button.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { PrimaryButtonComponent } from './primary-button.component';
 
-export const primaryButtonModuleImports = [...([CommonModule, FontAwesomeModule] as const)];
+export const primaryButtonModuleImports = [...([CommonModule, FontAwesomeModule, FlexLayoutModule] as const)];
 
 @NgModule({
     imports: primaryButtonModuleImports,
