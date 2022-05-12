7b4e7c16dd0dbc4fcf066a85e82e045c8e9e9133
Jordan Emslie <jordems@gmail.com>
2022-05-11 21:11:14 -0700

    fix(backend feature table): resolve circular dependancy with turn timer service (#175)

--- libs/backend/feature/table/src/lib/shared/turn-timer/turn-timer.module.ts
+++ libs/backend/feature/table/src/lib/shared/turn-timer/turn-timer.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { JobSchedulerModule } from '@poker-moons/backend/shared/service/job-scheduler';
-import { PlayerActionModule } from '../../player-action/player-action.module';
 import { TableStateManagerModule } from '../../table-state-manager/table-state-manager.module';
 import { TableGatewayModule } from '../websocket/table-gateway.module';
 import { TurnTimerService } from './turn-timer.service';
 
 @Module({
-    imports: [JobSchedulerModule, TableGatewayModule, TableStateManagerModule, PlayerActionModule],
+    imports: [JobSchedulerModule, TableGatewayModule, TableStateManagerModule],
     providers: [TurnTimerService],
     exports: [TurnTimerService],
 })
