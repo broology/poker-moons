4adb17ef7fff342fa66a150f8c25ea282fa2b225
kevinfeddema <kfeddema25@gmail.com>
2022-05-17 22:47:06 -0600

    chore(frontent): removed test code

--- libs/frontend/builder/feature/form/src/lib/frontend-builder-form-ui/frontend-builder-form-ui.module.ts
+++ libs/frontend/builder/feature/form/src/lib/frontend-builder-form-ui/frontend-builder-form-ui.module.ts
@@ -1,10 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { SecondaryButtonModule } from '@poker-moons/frontend/shared/ui';
 import { FrontendBuilderFormUiComponent } from './frontend-builder-form-ui.component';
 
-export const frontendBuilderFormUiImports = [...([CommonModule, ReactiveFormsModule, SecondaryButtonModule] as const)];
+export const frontendBuilderFormUiImports = [...([CommonModule, ReactiveFormsModule] as const)];
 
 @NgModule({
     imports: frontendBuilderFormUiImports,
