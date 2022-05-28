52f8b3c900dbf84c5617f065555b19a1091cf92f
Jordan Emslie <jordems@gmail.com>
2022-05-28 07:11:20 -0700

    feat(frontend shared ui): create general dialog component (#178)
    * feat(frontend shared ui): create general dialog component
    
    * feat(frontend builder feature-form): add poker-moons dialog to builder form
    
    * fix(frontend shared ui): failing unit test for dialog

--- libs/frontend/builder/feature/form/src/lib/frontend-builder-form-ui/frontend-builder-form-ui.module.ts
+++ libs/frontend/builder/feature/form/src/lib/frontend-builder-form-ui/frontend-builder-form-ui.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { DialogModule } from '@poker-moons/frontend/shared/ui';
 import { FrontendBuilderFormUiComponent } from './frontend-builder-form-ui.component';
 
-export const frontendBuilderFormUiImports = [...([CommonModule, ReactiveFormsModule] as const)];
+export const frontendBuilderFormUiImports = [...([CommonModule, ReactiveFormsModule, DialogModule] as const)];
 
 @NgModule({
     imports: frontendBuilderFormUiImports,
