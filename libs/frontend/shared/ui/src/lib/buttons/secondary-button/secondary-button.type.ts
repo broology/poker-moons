6373e1b8e655e957a8f0b2664638eedf8a7ca4c5
kevinfeddema <kfeddema25@gmail.com>
2022-06-02 21:48:41 -0600

    fix(frontent): seperate lib for styles utils

--- libs/frontend/shared/ui/src/lib/buttons/secondary-button/secondary-button.type.ts
+++ libs/frontend/shared/ui/src/lib/buttons/secondary-button/secondary-button.type.ts
@@ -1,4 +1,4 @@
-import { mainColors } from '@poker-moons/styles';
+import { mainColors } from '@poker-moons/frontend/shared/util/styles';
 
 export const variants = ['primary', 'secondary'] as const;
 
