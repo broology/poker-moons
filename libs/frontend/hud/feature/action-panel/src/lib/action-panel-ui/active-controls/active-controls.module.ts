ef12b4ee9be8e3eceeb24716cbabae7193372341
jordems <jordems@gmail.com>
2022-06-11 11:09:33 -0700

    feat(frontend hud feature action-panel): style action controls

--- libs/frontend/hud/feature/action-panel/src/lib/action-panel-ui/active-controls/active-controls.module.ts
+++ libs/frontend/hud/feature/action-panel/src/lib/action-panel-ui/active-controls/active-controls.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HudFrameModule } from '../../shared';
 import { ActiveControlsComponent } from './active-controls.component';
 
-export const activeControlsImports = [...([CommonModule, ReactiveFormsModule] as const)];
+export const activeControlsImports = [...([CommonModule, ReactiveFormsModule, HudFrameModule] as const)];
 
 @NgModule({
     imports: activeControlsImports,
