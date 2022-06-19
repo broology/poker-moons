ae745b01dff782f9b9c84af0af1393696c02cd10
kevinfeddema <kfeddema25@gmail.com>
2022-06-19 08:27:41 -0600

    feat(frontend): init timer lib
    (cherry picked from commit 887b3e9399f30df61128be728b32ae5762e20bf1)

---
+++ libs/frontend/shared/timer-moon/jest.config.ts
@@ -0,0 +1,15 @@
+module.exports = {
+    displayName: 'frontend-shared-timer-moon',
+    preset: '../../../../jest.preset.ts',
+    globals: {
+        'ts-jest': {
+            tsconfig: '<rootDir>/tsconfig.spec.json',
+        },
+    },
+    testEnvironment: 'node',
+    transform: {
+        '^.+\\.[tj]sx?$': 'ts-jest',
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+    coverageDirectory: '../../../../coverage/libs/frontend/shared/timer-moon',
+};
