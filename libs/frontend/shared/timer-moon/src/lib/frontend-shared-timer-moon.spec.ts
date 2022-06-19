e86d7fb16c1a0d7b4eeec2a8227fafe3d4ea0077
kevinfeddema <kfeddema25@gmail.com>
2022-06-19 08:27:41 -0600

    feat(frontend): init timer lib
    (cherry picked from commit 43fe08ad6e52c2b6cd1a787629e63cd3c73bec57)

---
+++ libs/frontend/shared/timer-moon/src/lib/frontend-shared-timer-moon.spec.ts
@@ -0,0 +1,7 @@
+import { frontendSharedTimerMoon } from './frontend-shared-timer-moon';
+
+describe('frontendSharedTimerMoon', () => {
+    it('should work', () => {
+        expect(frontendSharedTimerMoon()).toEqual('frontend-shared-timer-moon');
+    });
+});
