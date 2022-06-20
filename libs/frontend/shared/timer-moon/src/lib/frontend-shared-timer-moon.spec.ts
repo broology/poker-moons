e2151a63232e272c38cfb7b9bd5247db3ec819aa
kevinfeddema <kfeddema25@gmail.com>
2022-06-20 16:23:03 -0600

    fix(frontend): re-init moon timer comp
    (cherry picked from commit 320691492b9210e4e58b15633f4037c213c1a63f)

--- libs/frontend/shared/timer-moon/src/lib/frontend-shared-timer-moon.spec.ts
+++
@@ -1,7 +0,0 @@
-import { frontendSharedTimerMoon } from './frontend-shared-timer-moon';
-
-describe('frontendSharedTimerMoon', () => {
-    it('should work', () => {
-        expect(frontendSharedTimerMoon()).toEqual('frontend-shared-timer-moon');
-    });
-});
