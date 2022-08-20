68a9b73a19aefc52d0231406b8d48fc606ff03ba
Jordan Emslie <jordems@gmail.com>
2022-08-20 16:07:08 -0700

    fix(cards): change card meister implemention to fix rendering issues (#208)

--- libs/frontend/shared/ui/src/lib/card/meister/card.adapter.ts
+++ libs/frontend/shared/ui/src/lib/card/meister/card.adapter.ts
@@ -1,8 +1,8 @@
 import { Card, Rank } from '@poker-moons/shared/type';
-import { gen } from './elements.cardmeister.min';
 
 /**
- * Interface of parameters that can be applied to the meister `card-t`.
+ * @description Interface of parameters that can be applied to the meister `card-t`.
+ *
  * - Only added the ones being used so far.
  *
  * @see https://github.com/cardmeister/cardmeister.github.io for more parameters
@@ -15,11 +15,11 @@ interface MeisterCardAttr {
 }
 
 /**
- * Converts the PokerMoons `Rank` to the Meister library rank value.
+ * @description Converts the PokerMoons `Rank` to the Meister library rank value.
  *
- * @param rank - PokerMoons `Rank` of card
+ * @param rank - PokerMoons `Rank` of card.
  *
- * @returns - Meister `Rank` of card
+ * @returns - Meister `Rank` of card.
  */
 function rankToCartMeisterRank(rank: Rank): string {
     switch (rank) {
@@ -32,12 +32,13 @@ function rankToCartMeisterRank(rank: Rank): string {
 }
 
 /**
- * Converts the card data we store to parameters for meister `card-t`'s card.
+ * @description Converts the card data we store to parameters for meister `card-t`'s card.
+ *
+ * @param card - The suite and the rank to be displayed. If null back of card.
+ *
+ * @returns - List of meister card paramaters to create the `card`
  *
  * @see https://github.com/cardmeister/cardmeister.github.io for more details
- *
- * @param card - the suite and the rank to be displayed. If null back of card
- * @returns - list of meister card paramaters to create the `card`
  */
 export function cardToCardMeister(card: Card | null): MeisterCardAttr {
     if (card === null) {
@@ -54,11 +55,3 @@ export function cardToCardMeister(card: Card | null): MeisterCardAttr {
         rank: rankToCartMeisterRank(card.rank),
     };
 }
-
-/**
- * Applies the cardmeister script onto the image element.
- * Which must already have all the necessary attributes created in {@link cardToCardMeister}
- *
- * @param ref - The html image element
- */
-export const applyMeisterCard = (ref: HTMLImageElement) => gen(ref);
