877d6643013b56f83f41c57cd634a9ab52641e1c
Jordan Emslie <jordems@gmail.com>
2022-08-20 13:47:42 -0700

    fix(action-panel): change to function the same way as poker-starts (#204)
    * build(.prettierrc): add auto import organization and js doc code formatting
    
    * feat(frontend shared ui): create shared chip currency pipe transform
    
    * chore(action-panel): update raise value to update dynamically and add values to actions

--- libs/frontend/shared/state/table/src/lib/actions/ws.actions.ts
+++ libs/frontend/shared/state/table/src/lib/actions/ws.actions.ts
@@ -2,21 +2,21 @@ import { ConnectedEvent, PokerMoonsError, TableEvent, TableId } from '@poker-moo
 import { ActionType, buildAsyncRequestActions, createActionType } from '../shared/util/action-util';
 
 /**
- * On `initialize` attempt to connect to websocket
+ * @description On `initialize` attempt to connect to websocket.
  */
 export const connectToWs = buildAsyncRequestActions<{ tableId: TableId }, ConnectedEvent, PokerMoonsError>(
     'connect to table websocket',
 );
 
 /**
- * Type map to enforce that all websocket events are handled on the frontend.
+ * @description Type map to enforce that all websocket events are handled on the frontend.
  */
 export type TableWsActionMap = {
     [type in Exclude<TableEvent['type'], 'connected'>]: ActionType<Extract<TableEvent, { type: type }>>;
 };
 
 /**
- * Websocket actions that are pushed to the client
+ * @description Websocket actions that are pushed to the client.
  */
 export const tableWsActionMap: TableWsActionMap = {
     playerJoined: createActionType('player has joined the table'),
--- libs/frontend/underlay/feature/seat-layout/src/lib/ui/seat/seat-player/seat-player.module.ts
+++ libs/frontend/underlay/feature/seat-layout/src/lib/ui/seat/seat-player/seat-player.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ChipCurrencyModule } from '@poker-moons/frontend/shared/ui';
 import { AssetUrlModule } from '@poker-moons/frontend/shared/util/environment';
 import { SeatPlayerComponent } from './seat-player.component';
 
-export const seatPlayerImports = [...([CommonModule, AssetUrlModule] as const)];
+export const seatPlayerImports = [...([AssetUrlModule, CommonModule, ChipCurrencyModule] as const)];
 
 @NgModule({
     imports: seatPlayerImports,
