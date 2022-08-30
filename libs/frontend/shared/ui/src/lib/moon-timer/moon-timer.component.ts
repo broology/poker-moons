82ba66350dbfe9b3f785d8a418be52382ce5f167
kevinfeddema <kfeddema25@gmail.com>
2022-08-30 01:06:34 -0600

    feat(frontend): iinitial implementation of timer

--- libs/frontend/shared/ui/src/lib/moon-timer/moon-timer.component.ts
+++ libs/frontend/shared/ui/src/lib/moon-timer/moon-timer.component.ts
@@ -9,15 +9,49 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 })
 export class MoonTimerComponent implements OnInit {
     @Input() duration: number;
-    @Input() currentTime: number;
+    currentTime: number;
+    displayTime: string;
     @Output() handleClick: EventEmitter<string> = new EventEmitter<string>();
 
     constructor() {
-        this.duration = 60;
+        this.duration = 20000;
         this.currentTime = this.duration;
+        this.displayTime = this.getDisplayTime(this.currentTime);
     }
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.start();
+    }
+
+    getDisplayTime(time: number): string {
+        const min = Math.floor((time / 1000 / 60) % 60);
+        const sec = Math.floor((time / 1000) % 60);
+
+        return [min.toString().padStart(2, '0'), sec.toString().padStart(2, '0')].join(':');
+    }
+
+    getPath(): string {
+        const toSubtract = ((this.duration - this.currentTime) / this.duration) * 130;
+
+        return `M50,100 C${115 - toSubtract},100 ${115 - toSubtract},0 50,0 C-15,0 -15,100 50,100`;
+    }
+
+    start(): void {
+        setInterval(() => {
+            if (this.currentTime > 0) {
+                const time = this.currentTime - 1000;
+                this.currentTime = time;
+                this.displayTime = this.getDisplayTime(time);
+            } else {
+                clearInterval();
+            }
+        }, 1000);
+    }
+
+    getColorClass(): string {
+        const ratio = this.currentTime / this.duration;
+        return ratio > 0.5 ? 'success' : ratio > 0.25 ? 'warning' : 'error';
+    }
 
     click(): void {
         return this.handleClick.emit();
