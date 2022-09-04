62fcbe271d44df41950dc3a4ad4bc3fbe8b1028c
jordems <jordems@gmail.com>
2022-09-04 10:05:36 -0700

    refactor(frontend hud feature bank-panel): remove unused feature

--- libs/frontend/hud/feature/bank-panel/src/lib/bank-panel-feature.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'poker-moons-bank-panel-feature',
-    templateUrl: './bank-panel-feature.component.html',
-    styleUrls: ['./bank-panel-feature.component.scss'],
-})
-export class BankPanelFeatureComponent {}
--- libs/frontend/hud/shell/src/lib/frontend-hud-shell.module.ts
+++ libs/frontend/hud/shell/src/lib/frontend-hud-shell.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { ActionPanelFeatureModule } from '@poker-moons/frontend/hud/feature/action-panel';
-import { BankPanelFeatureModule } from '@poker-moons/frontend/hud/feature/bank-panel';
 import { FrontendHudShellComponent } from './frontend-hud-shell.component';
 @NgModule({
-    imports: [ActionPanelFeatureModule, BankPanelFeatureModule],
+    imports: [ActionPanelFeatureModule],
     declarations: [FrontendHudShellComponent],
     exports: [FrontendHudShellComponent],
 })
