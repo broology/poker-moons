5d5834ff1294f6be871494df72fb65ae8de1259f
kevinfeddema <kfeddema25@gmail.com>
2022-09-07 21:36:24 -0600

    fix(frontend): rebuilt with observables
    (cherry picked from commit 997945b5e75f7ae073e5295a79d2701a56b12ea4)

--- libs/frontend/landing/src/lib/frontend-landing.module.ts
+++ libs/frontend/landing/src/lib/frontend-landing.module.ts
@@ -5,6 +5,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
 import { LandingButtonModule } from '@poker-moons/frontend/shared/ui';
 import { AssetUrlModule } from '@poker-moons/frontend/shared/util/environment';
+import { SeatPlayerModule } from 'libs/frontend/underlay/feature/seat-layout/src/lib/ui/seat/seat-player/seat-player.module';
 import { FrontendLandingComponent } from './frontend-landing.component';
 import { TableBuilderStore } from './store/table-builder.store';
 
@@ -15,6 +16,7 @@ export const frontendLandingImports = [
         FlexLayoutModule,
         LandingButtonModule,
         HttpClientModule,
+        SeatPlayerModule,
         RouterModule.forChild([
             {
                 path: '',
--- libs/frontend/shared/ui/src/lib/moon-timer/moon-timer.component.ts
+++ libs/frontend/shared/ui/src/lib/moon-timer/moon-timer.component.ts
@@ -1,6 +1,14 @@
 /* eslint-disable @angular-eslint/no-empty-lifecycle-method */
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { interval, Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+interface Time {
+    total: number;
+    min: string;
+    sec: string;
+}
 
 @Component({
     selector: 'poker-moons-moon-timer',
@@ -10,44 +18,33 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 export class MoonTimerComponent implements OnInit {
     @Input() duration: number;
     currentTime: number;
-    displayTime: string;
+    public currentTime$: Observable<Time>;
     @Output() handleClick: EventEmitter<string> = new EventEmitter<string>();
 
     constructor() {
         this.duration = 20000;
         this.currentTime = this.duration;
-        this.displayTime = this.getDisplayTime(this.currentTime);
+        this.currentTime$ = interval(1000).pipe(
+            map(() => this.getTime()),
+            shareReplay(1),
+        );
     }
 
-    ngOnInit(): void {
-        this.start();
+    ngOnInit(): void {}
+
+    getTime(): Time {
+        this.currentTime$.pipe().subscribe((val) => (this.currentTime = val.total - 1000));
+
+        const min = Math.floor((this.currentTime / 1000 / 60) % 60);
+        const sec = Math.floor((this.currentTime / 1000) % 60);
+
+        return { total: this.currentTime, min: min.toString().padStart(2, '0'), sec: sec.toString().padStart(2, '0') };
     }
-
-    getDisplayTime(time: number): string {
-        const min = Math.floor((time / 1000 / 60) % 60);
-        const sec = Math.floor((time / 1000) % 60);
-
-        return [min.toString().padStart(2, '0'), sec.toString().padStart(2, '0')].join(':');
-    }
-
     getPath(): string {
         const toSubtract = ((this.duration - this.currentTime) / this.duration) * 130;
 
         return `M50,100 C${115 - toSubtract},100 ${115 - toSubtract},0 50,0 C-15,0 -15,100 50,100`;
     }
-
-    start(): void {
-        setInterval(() => {
-            if (this.currentTime > 0) {
-                const time = this.currentTime - 1000;
-                this.currentTime = time;
-                this.displayTime = this.getDisplayTime(time);
-            } else {
-                clearInterval();
-            }
-        }, 1000);
-    }
-
     getColorClass(): string {
         const ratio = this.currentTime / this.duration;
         return ratio > 0.5 ? 'good' : ratio > 0.25 ? 'warning' : 'danger';
