39586ea41a133d9b1493075b59cb1884ed5394ed
kevinfeddema <kfeddema25@gmail.com>
2022-09-12 22:31:33 -0600

    fix(frontend): attempt to fix broken state

--- libs/frontend/shared/ui/src/lib/moon-timer/moon-timer.component.ts
+++ libs/frontend/shared/ui/src/lib/moon-timer/moon-timer.component.ts
@@ -16,29 +16,39 @@ interface Time {
     styleUrls: ['./moon-timer.component.scss'],
 })
 export class MoonTimerComponent implements OnInit {
-    @Input() duration: number;
-    currentTime: number;
+    @Input() width = '100%';
+    @Input() height = '100%';
+    @Input() duration = 30000;
+    private currentTime: number;
     public currentTime$: Observable<Time>;
     @Output() handleClick: EventEmitter<string> = new EventEmitter<string>();
 
     constructor() {
-        this.duration = 20000;
-        this.currentTime = this.duration;
+        this.currentTime = 0;
         this.currentTime$ = interval(1000).pipe(
             map(() => this.getTime()),
             shareReplay(1),
         );
     }
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.currentTime = this.duration;
+        this.currentTime$.pipe().subscribe((val) => (this.currentTime = val.total - 1000));
+    }
 
     getTime(): Time {
-        this.currentTime$.pipe().subscribe((val) => (this.currentTime = val.total - 1000));
+        if (this.currentTime <= 0) {
+            return { total: 0, min: '00', sec: '00' };
+        }
 
         const min = Math.floor((this.currentTime / 1000 / 60) % 60);
         const sec = Math.floor((this.currentTime / 1000) % 60);
 
-        return { total: this.currentTime, min: min.toString().padStart(2, '0'), sec: sec.toString().padStart(2, '0') };
+        return {
+            total: this.currentTime,
+            min: min.toString().padStart(2, '0'),
+            sec: sec.toString().padStart(2, '0'),
+        };
     }
     getPath(): string {
         const toSubtract = ((this.duration - this.currentTime) / this.duration) * 130;
@@ -46,10 +56,6 @@ export class MoonTimerComponent implements OnInit {
         return `M50,100 C${115 - toSubtract},100 ${115 - toSubtract},0 50,0 C-15,0 -15,100 50,100`;
     }
     getColorClass(): string {
-        const ratio = this.currentTime / this.duration;
-        return ratio > 0.5 ? 'good' : ratio > 0.25 ? 'warning' : 'danger';
-    }
-    getMoonColorClass(): string {
         const ratio = this.currentTime / this.duration;
         return ratio > 0.5 ? 'moon' : ratio > 0.25 ? 'lunar-eclipse' : 'solar-eclipse';
     }
