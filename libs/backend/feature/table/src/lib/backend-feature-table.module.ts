702e62934c54ce0c3fec9cab658b6ecb1da6cfe8
jordems <jordems@gmail.com>
2022-11-26 15:12:45 -0800

    refactor(backend-feature-table): decouple route config from module

--- libs/backend/feature/table/src/lib/backend-feature-table.module.ts
+++ libs/backend/feature/table/src/lib/backend-feature-table.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { RouterModule } from '@nestjs/core';
+import { RouterModule, Routes } from '@nestjs/core';
 import { PlayerActionModule } from './player-action/player-action.module';
 import { PlayerModule } from './player/player.module';
 import { ReadySystemConsumerModule } from './shared/ready-system';
@@ -8,6 +8,38 @@ import { TurnTimerConsumerModule } from './shared/turn-timer/consumer/turn-timer
 import { TurnTimerModule } from './shared/turn-timer/turn-timer.module';
 import { TableGatewayModule } from './shared/websocket/table-gateway.module';
 import { TableModule } from './table/table.module';
+
+/**
+ * @description Routes defined under `/table`.
+ */
+const routes: Routes = [
+    {
+        path: 'table',
+        module: TableModule,
+        children: [
+            {
+                path: ':tableId',
+                children: [
+                    {
+                        path: 'player',
+                        module: PlayerModule,
+                        children: [
+                            {
+                                path: ':playerId',
+                                children: [
+                                    {
+                                        path: 'action',
+                                        module: PlayerActionModule,
+                                    },
+                                ],
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+    },
+];
 
 @Module({
     imports: [
@@ -21,34 +53,7 @@ import { TableModule } from './table/table.module';
         TurnTimerConsumerModule,
 
         // Routes
-        RouterModule.register([
-            {
-                path: 'table',
-                module: TableModule,
-                children: [
-                    {
-                        path: ':tableId',
-                        children: [
-                            {
-                                path: 'player',
-                                module: PlayerModule,
-                                children: [
-                                    {
-                                        path: ':playerId',
-                                        children: [
-                                            {
-                                                path: 'action',
-                                                module: PlayerActionModule,
-                                            },
-                                        ],
-                                    },
-                                ],
-                            },
-                        ],
-                    },
-                ],
-            },
-        ]),
+        RouterModule.register(routes),
 
         // Shared Services
         ReadySystemModule,
